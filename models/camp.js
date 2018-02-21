73685a396e452b04926e28f076fca059f25e2a85
Jesús Lazo <jesus.lazo@tecnoandina.cl>
2018-02-21 13:42:06 -0300

    Cambios a las acciones de predios

--- models/camp.js
+++ models/camp.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 const CampSchema = Schema({
     name: { type: String, required: false },
-    location: { type: [Number], unique: true, required: false },
+    location: { type: [Number], required: false },
     client: { type: Schema.ObjectId, ref: 'User',required: true }
 });
 CampSchema.plugin(uniqueValidator, { message: 'La ubicación debe ser unica' });
